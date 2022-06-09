79c10d4f612e6c71a8986aeff391d89f32d7604e
Paul Wilson <paul.wilson.r@gmail.com>
2022-06-09 11:24:38 -0500

    finishes project with js added in.

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-let errorClassArray = document.querySelectorAll('.password');
+const passwordContainer = document.querySelectorAll('.passwordContainer');
+const passwordArray = document.querySelectorAll('.password');
 
-function checkPasswords(errorClassArray) { //checks if the passwords match
-    return(errorClassArray[0].value === errorClassArray[1].value ? true : false);
+function checkPasswords() { //checks if the passwords match
+    if(passwordArray[0].value !== passwordArray[1].value){
+        addError();
+    } else {removeError();
+    };
 };
 
-function errorBox() { //changes the input box between error and not
-    
-}
+function addError() { //changes the input box between error and not
+    if(!passwordContainer[0].classList.contains('error')){
+        passwordContainer.forEach(element => {
+            element.classList.add("error");
 
+            });
+    }
+};
 
-console.log(checkPasswords(errorClassArray))
+function removeError() {
+    if(passwordContainer[0].classList.contains('error')){
+        passwordContainer.forEach(element => {
+            element.classList.remove("error");
+
+            });
+    }
+};
+
+passwordContainer[1].addEventListener('change',() => {
+    passwordContainer.forEach(element => {
+        element.addEventListener('change', checkPasswords)
+    }
+    )
+    checkPasswords();    
+}, {once : true});
+
+/*passwordContainer.forEach(element => {
+    element.addEventListener('change', checkPasswords);
+});
+*/
